179a2f50c55303ef44b5e10b727a303430ad35e0
nguyenbaolong11062000 <52575643+nguyenbaolong11062000@users.noreply.github.com>
2020-12-04 11:45:39 +0700

    template

---
+++ nha_sach/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.secret_key = '3^@&@&!&(@*UGUIEIU&@^!*(@&*(SS'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Long1162000@@localhost/dbNhaSach1?charset=utf8'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app=app)
+admin = Admin(app=app, name='LT BookStore',
+              template_mode="bootstrap4")
+login = LoginManager(app=app)
